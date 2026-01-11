55fdf796997d22a2868994d9a0710f8a0d186096
Guillaume de Puget <guillaumedepuget@gmail.com>
2026-01-11 21:53:37 +0100

    chore: initial cleanup and dependency update

---
+++ src/main/java/com/openclassrooms/tourguide/config/TourGuideModule.java
@@ -0,0 +1,29 @@
+package com.openclassrooms.tourguide.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.openclassrooms.tourguide.services.RewardsService;
+
+import gpsUtil.GpsUtil;
+import rewardCentral.RewardCentral;
+
+@Configuration
+public class TourGuideModule {
+	
+	@Bean
+	public GpsUtil getGpsUtil() {
+		return new GpsUtil();
+	}
+	
+	@Bean
+	public RewardsService getRewardsService() {
+		return new RewardsService(getGpsUtil(), getRewardCentral());
+	}
+	
+	@Bean
+	public RewardCentral getRewardCentral() {
+		return new RewardCentral();
+	}
+	
+}
