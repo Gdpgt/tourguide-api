682b65c5d50391960276caad1a355ec35416ba7b
Guillaume de Puget <guillaumedepuget@gmail.com>
2026-01-29 22:19:12 +0100

    fix: nearAllAttractions test, by fixing user entity

--- src/main/java/com/openclassrooms/tourguide/services/RewardsService.java
+++ src/main/java/com/openclassrooms/tourguide/services/RewardsService.java
@@ -43,10 +43,10 @@ public class RewardsService {
 		
 		for(VisitedLocation visitedLocation : userLocations) {
 			for(Attraction attraction : attractions) {
-				if(user.getUserRewards().stream().filter(r -> r.attraction.attractionName.equals(attraction.attractionName)).count() == 0) {
-					if(nearAttraction(visitedLocation, attraction)) {
-						user.addUserReward(new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
-					}
+				if(nearAttraction(visitedLocation, attraction)) {
+					user.getUserRewardsAsMap()
+						.computeIfAbsent(attraction.attractionName, 
+										 a -> new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
 				}
 			}
 		}
